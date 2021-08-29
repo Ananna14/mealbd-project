c8b6eec9a28458f49ffed88e036f3e07f65f7ab0
Ananna14 <anannasheikh89@gmail.com>
2021-08-29 05:04:30 -0700

    first commit

---
+++ js/meal.js
@@ -0,0 +1,54 @@
+const searchFood = () => {
+    const searchInput = document.getElementById('searchInput');
+    const searchText = searchInput.value;
+    searchInput.value = '';
+
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => diplaySearchResult(data.meals));
+
+
+}
+
+const diplaySearchResult = meals => {
+    const searchResult = document.getElementById('search-result');
+    meals.forEach(meal => {
+        // console.log(meal);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div onclick="loadMealDetail(${meal.idMeal})" class="card">
+        <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${meal.strMeal}</h5>
+            <p class="card-text">${meal.strInstructions.slice(0, 200)}</p>
+        </div>
+        `;
+
+        searchResult.appendChild(div);
+    });
+}
+
+const loadMealDetail = mealId => {
+    // console.log(mealId);
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId} `;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => displayMealDetail(data.meals[0]));
+}
+
+const displayMealDetail = meal => {
+    console.log(meal);
+    const mealDetail = document.getElementById('meal-details');
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = ` <img src='${meal.strMealThumb}' class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">${meal.strMeal}</h5>
+        <p class="card-text">${meal.strInstructions.slice(0, 200)}</p>
+        <a href="${meal.strYoutube}" class="btn btn-primary">Go somewhere</a>
+        
+    </div>`
+    mealDetail.appendChild(div);
+}
